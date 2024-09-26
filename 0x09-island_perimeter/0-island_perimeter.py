c9853181e2561f7cae5bd9d018eb6284487be10d
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-09-26 02:10:33 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,18 +6,18 @@
 
 def island_perimeter(grid):
     """Returns the perimeter described in grid."""
-    perimeter = 0
-    
+    prmt = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
+                    prmt += 1
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
+                    prmt += 1
                 if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
+                    prmt += 1
                 if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+                    prmt += 1
 
-    return perimeter
+    return prmt
